d54973e05f612e8a11ee9c276ebb488948ff2618
Axyaani <axyaani@gmail.com>
2025-07-16 07:05:18 +0300

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
 
   return (
     <div className="flex justify-between px-20 pt-5 shadow-lg py-6 text-3xl">
-    <div className='flex gap-16'> 
+    <div className='flex gap-8 -ml-9'> 
         <img className='w-16 -mt-2' src={logo} alt="" />
-          <h1 className='text-4xl -ml-4'> Zain Coffee</h1>
+          <h1 className='text-4xl '> Zain Coffe</h1>
 
     </div>
-   <i className="fa-solid fa-bars sm:hidden ml-44"></i>
+   <i className="fa-solid fa-bars sm:hidden ml-20 mt-4"></i>
       <div>
         <ul className='sm:flex gap-4 hidden'>
          <Link to="/"><li> Home</li></Link> 
